6c9fee2a1635fe707984d7751afad21b556c6e7c
potash <eric@k2co3.net>
2015-09-23 20:50:54 +0000

    feature impotance

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import datetime
 from sklearn import metrics
 import pandas as pd
+import numpy as np
 import math
 from statsmodels.discrete.discrete_model import Logit
 
@@ -53,6 +54,17 @@ def summary(name, max_train_age, date, y_train, y_test, y_score):
 def sk_tree(X,y, params={'max_depth':3}):
     clf = tree.DecisionTreeClassifier(**params)
     return clf.fit(X, y)
+
+def feature_importance(estimator, X):
+    if hasattr(estimator, 'coef_'):
+        i = estimator.coef_[0]
+    elif hasattr(estimator, 'feature_importances_'):
+        i = estimator.feature_importances_
+    else:
+        i = [np.nan]*len(X.columns)
+
+    return pd.DataFrame({'feature': X.columns, 'importance': i}).sort('importance')
+
 
 class LogisticRegression(object):
     def __init__(self):
