0729c3027bb7f4118c5f1f6282dbeb57e539a8e8
potash <eric@k2co3.net>
2015-10-02 20:03:53 +0000

    feature sort order

--- model.py
+++ model.py
@@ -24,7 +24,7 @@ def feature_importance(estimator, X):
     else:
         i = [np.nan]*len(X.columns)
 
-    return pd.DataFrame({'feature': X.columns, 'importance': i}).sort('importance')
+    return pd.DataFrame({'feature': X.columns, 'importance': i}).sort('importance', ascending=False)
 
 
 class LogisticRegression(object):
