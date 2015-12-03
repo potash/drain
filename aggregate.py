0bac2ee1d54adb0a43122c39066671b300cd4c9f
potash <eric@k2co3.net>
2015-12-03 19:49:24 +0000

    spacedeltas

--- aggregate.py
+++ aggregate.py
@@ -1,6 +1,10 @@
 import os
-from datetime import date
+import re
 import logging
+
+import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 import pandas as pd
 import numpy as np
@@ -72,9 +76,9 @@ def __series(df, attr):
 def censor(df, date_column, end_date, delta):
     df = df[ df[date_column] < end_date ]
 
-    if delta != -1:
-        start_date = end_date - np.timedelta64(delta*365, 'D')
-        df = df[ df[date_column] > start_date ]
+    if delta is not None:
+        start_date = end_date - delta
+        df = df[ df[date_column] >= start_date ]
 
     return df
 
@@ -87,10 +91,8 @@ def aggregate_set(l):
 def aggregate_counts(l):
     return np.unique(np.concatenate(l.values), return_counts=True)
 
-# spacetimes is a list of space names and deltas to aggregate,
-#     e.g. ['address' : [1,2,3], 'tract':[4,5,6]
-# spatial indexes the corresponding list of spatial index columns
-#     e.g. ['address_id', 'census_tract_id']
+# spacetimes is a dict of space_name : Spacetimes 
+#     e.g. ['industry_state' : Spacetime(['naics_code', 'state'], ['1y', '5y', 'all'])
 # dates is a collection of dates to aggregate (all the spacetimes) to
 #     e.g. [date(2012,1,1), date(2013,1,1)]
 # basedir is the base directory for storing hdf files
@@ -103,9 +105,8 @@ def aggregate_counts(l):
 # TODO prefix column
 # TODO test table vs fixed format, data_column
 class SpacetimeAggregator(object):
-    def __init__(self, spacetimes, spatial_indexes, dates, prefix, basedir):
-        self.spacetimes = spacetimes
-        self.spatial_indexes = spatial_indexes
+    def __init__(self, spacedeltas, dates, prefix, basedir):
+        self.spacedeltas = spacedeltas
         self.prefix = prefix
         self.dates = dates
         self.dirname = os.path.join(basedir, prefix)
@@ -151,3 +152,25 @@ class SpacetimeAggregator(object):
             os.mkdir(self.dirname)
         df = df.astype(np.float32) if dtype is not None else df
         return df.to_hdf(self.filenames[date], key='df', mode='w')
+
+delta_chars = {
+        'y':'years', 'm':'months', 'w':'weeks', 'd':'days', 'h':'hours', 
+        'M':'minutes', 's':'seconds', 'u':'microseconds'
+}
+
+delta_regex = re.compile('^([0-9]+)(u|s|M|h|d|m|y)$')
+
+def parse_delta(s):
+    if s == 'all':
+        return None
+    else:
+        l = delta_regex.findall(s)
+        if len(l) == 1:
+            return relativedelta(**{delta_chars[l[0][1]]:int(l[0][0])})
+        else:
+            raise ValueError('Invalid delta string: %s' % s)
+
+class Spacedeltas(object):
+    def __init__(self, spatial_index, delta_strings):
+        self.spatial_index = spatial_index
+        self.deltas = {s:parse_delta(s) for s in delta_strings}
