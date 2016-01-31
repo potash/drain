7065e2a5484f1e07975cda074c8aa0c034d245a3
Eric Potash <eric@k2co3.net>
2016-01-31 13:38:30 -0600

    test_select_date

--- test/test_data.py
+++ test/test_data.py
@@ -1,6 +1,9 @@
 from drain import data,step
 import tempfile
 import os
+from datetime import date
+import pandas as pd
+import numpy as np
 
 def setup_module(module):
     tmpdir = tempfile.mkdtemp()
@@ -8,14 +11,22 @@ def setup_module(module):
     step.configure_yaml()
 
 def test_to_hdf():
-   d = data.ClassificationData()
-   h = data.ToHDF(inputs=[d], target=True)
+    d = data.ClassificationData()
+    h = data.ToHDF(inputs=[d], target=True)
 
-   h.setup_dump()
-   step.run(h)
+    h.setup_dump()
+    step.run(h)
 
-   r0, r1 = h.get_result(), d.get_result()
+    r0, r1 = h.get_result(), d.get_result()
 
-   for key in r1.keys():
+    for key in r1.keys():
        assert r0[key].equals(r1[key])
-       
+
+def test_date_select():
+    df = pd.DataFrame({'date':[date(2013,m,1) for m in range(1,13)]})
+    assert np.array_equal(data.date_select(df, 'date', date(2013,4,1), 'all').values, df.values[0:3])
+
+    # test it on a pandas timestamp column too
+    df['date'] = pd.to_datetime(df['date'])
+    assert np.array_equal(data.date_select(df, 'date', date(2013,4,1), 'all').values, df.values[0:3])
+
