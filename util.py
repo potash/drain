60d159202da56c788d62cf8f0d419bfdbd899053
potash <eric@k2co3.net>
2016-06-27 04:09:23 +0000

    epoch in nanoseconds

--- util.py
+++ util.py
@@ -66,7 +66,7 @@ def timestamp(year,month,day):
     """
     return pd.Timestamp('%04d-%02d-%02d' % (year, month, day))
 
-epoch = np.datetime64(0, 'D')
+epoch = np.datetime64(0, 'ns')
 def date_to_days(date):
     """
     Number of days since epoch
