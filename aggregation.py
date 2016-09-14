c8f1271bcb45ee7f8ed955dec71b6142cf2a763d
potash <eric@k2co3.net>
2016-09-14 21:41:43 +0000

    explain spacetime agg join

--- aggregation.py
+++ aggregation.py
@@ -270,9 +270,13 @@ class SpacetimeAggregation(AggregationBase):
 
     def fillna_value(self, df, left, **kwargs):
         """
-        fills counts with zero
+        fills counts with zero.
+            TODO: identify counts more robustly instead of relying on column name
+        typically fill other fields with mean but can't do that during the join
+            because that would leak information across a train/test split
         """
-        value = pd.Series(0, index=[c for c in df.columns if c.endswith('_count') and c.find('_per_') == -1])
+        value = pd.Series(0, index=[c for c in df.columns if c.endswith('_count') 
+                                                             and c.find('_per_') == -1])
         return value
 
     @property
