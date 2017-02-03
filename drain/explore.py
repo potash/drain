0f6b3ff15dffb8b7709bfc64c5e75579537681ff
Eric Potash <eric@k2co3.net>
2017-02-03 17:21:14 +0000

    rm unused

--- drain/explore.py
+++ drain/explore.py
@@ -230,53 +230,6 @@ def intersection(df, pairwise=False, **subset_args):
 def apply_y(df, fn, **kwargs):
     return apply(df, lambda s: fn(model.y_subset(s.get_result()['y'], **kwargs)))
 
-# http://sensitivecities.com/so-youd-like-to-make-a-map-using-python-EN.html
-# Convenience functions for working with colour ramps and bars
-def colorbar_index(ncolors, cmap, labels=None, **kwargs):
-    """
-    This is a convenience function to stop you making off-by-one errors
-    Takes a standard colour ramp, and discretizes it,
-    then draws a colour bar with correctly aligned labels
-    """
-    cmap = cmap_discretize(cmap, ncolors)
-    mappable = cm.ScalarMappable(cmap=cmap)
-    mappable.set_array([])
-    mappable.set_clim(-0.5, ncolors+0.5)
-
-    import matplotlib.pyplot as plt
-    colorbar = plt.colorbar(mappable, **kwargs)
-    colorbar.set_ticks(np.linspace(0, ncolors, ncolors))
-    colorbar.set_ticklabels(range(ncolors))
-    if labels:
-        colorbar.set_ticklabels(labels)
-    return colorbar
-
-def cmap_discretize(cmap, N):
-    """
-    Return a discrete colormap from the continuous colormap cmap.
-
-        cmap: colormap instance, eg. cm.jet. 
-        N: number of colors.
-
-    Example
-        x = resize(arange(100), (5,100))
-        djet = cmap_discretize(cm.jet, 5)
-        imshow(x, cmap=djet)
-
-    """
-    if type(cmap) == str:
-        cmap = get_cmap(cmap)
-    colors_i = np.concatenate((np.linspace(0, 1., N), (0., 0., 0., 0.)))
-    colors_rgba = cmap(colors_i)
-    indices = np.linspace(0, 1., N + 1)
-    cdict = {}
-    for ki, key in enumerate(('red', 'green', 'blue')):
-        cdict[key] = [(indices[i], colors_rgba[i - 1, ki], colors_rgba[i, ki]) for i in xrange(N + 1)]
-    return matplotlib.colors.LinearSegmentedColormap(cmap.name + "_%d" % N, cdict, 1024)
-
-def jenks_labels(breaks):
-    return ["<= %0.1f (%s wards)" % (b, c) for b, c in zip(breaks.bins, breaks.counts)]
-
 def show_tree(tree, feature_names,max_depth=None):
     import wand.image
 
