c4775b2bdb9f2596c557bc73a3bf5c877d8532a4
Eric Potash <eric@k2co3.net>
2017-11-12 20:24:25 -0600

    make dict_product a little more convenient

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,8 +2,12 @@ from drain.util import *
 
 def test_dict_product():
     assert dict_product({1:[2,3], 4:[5,6]}) == [{1: 2, 4: 5}, {1: 2, 4: 6}, {1: 3, 4: 5}, {1: 3, 4: 6}]
+
 def test_dict_product_empty():
     assert dict_product({}) == [{}]
+
+def test_dict_product_kwargs():
+    assert dict_product({'a':1},b=[2,3]) == [{'a':1, 'b':2}, {'a':1, 'b':3}]
 
 def test_list_expand_basecase():
     assert list(list_expand([(1,2)])) == [(1,2)]
